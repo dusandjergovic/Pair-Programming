f849d6acd274072bb99f80181a4b3e145024b196
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-04-25 15:51:45 +0200

    so i deleted .svg, and imported .png, and I think it looks nice

---
+++ cenario/markup.js
@@ -0,0 +1,44 @@
+function createEl(tag, ClassName, value) {
+    let tagElement = document.createElement(`${tag}`)
+    tagElement.classList.add(`${ClassName}`);
+    tagElement.innerText = `${value}`;
+    return tagElement
+}
+
+const body = document.querySelector("body")
+
+const header = createEl('header', 'getStarted_navBar', '');
+body.appendChild(header)
+
+const divGetCenario = createEl('div', 'getCenario', '')
+const navBar = createEl('nav', 'navBar', '')
+header.appendChild(divGetCenario)
+header.appendChild(navBar)
+
+const eComm = createEl('span', 'spanText', 'eCommerce Funnel Analytics. Try it free now.');
+const btnForGetStarted = createEl('button', 'getStartedCenario', '');
+
+const spanForImg = createEl('span', 'spanForImg', '');
+const rocket = createEl('img', 'rocket', '');
+
+rocket.setAttribute('src', 'images/rocket.png')
+
+
+spanForImg.appendChild(rocket)
+
+const spanForText = createEl('span', 'spanForText', '');
+const textGetCenario = createEl('p', 'textGetCenario', 'Get Cenario')
+
+spanForText.appendChild(textGetCenario);
+
+btnForGetStarted.appendChild(spanForImg);
+btnForGetStarted.appendChild(spanForText);
+
+divGetCenario.appendChild(eComm);
+divGetCenario.appendChild(btnForGetStarted);
+
+
+
+
+
+
